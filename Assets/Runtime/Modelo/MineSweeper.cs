67be4a9767f27ebf66ebb49568b23a9fe27237cb
metalfurius <hackero1932@gmail.com>
2024-03-02 17:34:27 +0100

    Feat First methods

---
+++ Assets/Runtime/Modelo/MineSweeper.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineSweeper 
+{
+    public void Reveal(int x,int y)
+    {
+
+    }
+    public bool IsRevealed(int x, int y)
+    {
+        return true;
+    }
+}
--- Assets/Tests/NewTestScript.cs
+++ Assets/Tests/NewTestScript.cs
@@ -9,7 +9,7 @@ public class NewTestScript
     // A Test behaves as an ordinary method
     [Test]
     public void NewTestScriptSimplePasses() {
-        var sut = new Minesweeper();
+        var sut = new MineSweeper();
         sut.Reveal(4, 1);
         
         Assert.IsTrue(sut.IsRevealed(4, 1));
