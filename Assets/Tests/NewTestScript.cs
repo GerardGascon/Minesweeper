dd9a449008726cc81e41a65592abc0d1675414e0
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-03-02 19:09:25 +0100

    test: Changed naming

--- Assets/Tests/NewTestScript.cs
+++ Assets/Tests/NewTestScript.cs
@@ -8,14 +8,14 @@ public class NewTestScript
 {
     // A Test behaves as an ordinary method
     [Test]
-    public void NewTestScriptSimplePasses() {
+    public void RevealCell() {
         var sut = new MineSweeper(new Vector2Int(0, 0));
         sut.Reveal(4, 1);
         
         Assert.IsTrue(sut.IsRevealed(4, 1));
     }
     [Test]
-    public void asdaosdoj()
+    public void CellIsNotRevealed_ByDefault()
     {
         var sut = new MineSweeper(new Vector2Int(0, 0));
         Assert.IsFalse(sut.IsRevealed(5, 1));
@@ -32,19 +32,19 @@ public class NewTestScript
         Assert.IsFalse(sut.HasMineIn(3, 4));
     }
     [Test]
-    public void asdadadsa()
+    public void DetectOneMine_IfAdjacent()
     {
         var sut = new MineSweeper(new Vector2Int(2, 2));
         Assert.AreEqual(1, sut.CheckAdjacentMines(1, 1));
     }
     [Test]
-    public void asdadads2a()
+    public void DetectZeroMines_IfNotAdjacent()
     {
         var sut = new MineSweeper(new Vector2Int(2, 2));
         Assert.AreEqual(0, sut.CheckAdjacentMines(4, 4));
     }
     [Test]
-    public void jl32jlkk324()
+    public void GetCornerAdjacentCells()
     {
         var sut = new MineSweeper(new Vector2Int(2, 2));
         Assert.AreEqual(
@@ -52,7 +52,7 @@ public class NewTestScript
             sut.AdjacentOf(0, 0));
     }
     [Test]
-    public void aspdrfuke() {
+    public void GetNonCornerAdjacentCells() {
         var sut = new MineSweeper(new Vector2Int(1, 1));
         
         Assert.IsTrue(sut.AdjacentOf(1, 1).Contains(new Vector2Int(0, 0)));
@@ -65,7 +65,7 @@ public class NewTestScript
         Assert.IsTrue(sut.AdjacentOf(1, 1).Contains(new Vector2Int(2, 2)));
     }
     [Test]
-    public void fdsfsd5ff()
+    public void CheckIfTwoAdjacentMines()
     {
         var sut = new MineSweeper(new List<Vector2Int>{ new Vector2Int(0, 0), new Vector2Int(0, 2) });
         Assert.AreEqual(2, sut.CheckAdjacentMines(0, 1));
