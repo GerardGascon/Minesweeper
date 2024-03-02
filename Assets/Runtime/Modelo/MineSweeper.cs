620bdae30c2bcff1e0fcf4ac9b928c4250ac3f28
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-03-02 19:17:16 +0100

    refactor: Converted loop to linq

--- Assets/Runtime/Modelo/MineSweeper.cs
+++ Assets/Runtime/Modelo/MineSweeper.cs
@@ -1,68 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class MineSweeper
-{
-    private List<Vector2Int> cellsRevealed = new List<Vector2Int>();
-    private List<Vector2Int> mines = new List<Vector2Int>();
+public class MineSweeper {
+	private List<Vector2Int> cellsRevealed = new();
+	private List<Vector2Int> mines = new();
 
-    public MineSweeper(Vector2Int Mina)
-    {
-        mines.Add(Mina);
-    }
+	public MineSweeper(Vector2Int Mina) => mines.Add(Mina);
 
-    public MineSweeper(List<Vector2Int> minas)
-    {
-        mines.AddRange(minas);
-    }
+	public MineSweeper(List<Vector2Int> minas) => mines.AddRange(minas);
 
-    public void Reveal(int x, int y)
-    {
-        cellsRevealed.Add(new Vector2Int(x, y));
-    }
-    public bool IsRevealed(int x, int y)
-    {
-        return cellsRevealed.Contains(new Vector2Int(x, y));
-    }
+	public void Reveal(int x, int y) => cellsRevealed.Add(new Vector2Int(x, y));
+	public bool IsRevealed(int x, int y) => cellsRevealed.Contains(new Vector2Int(x, y));
 
-    public bool HasMineIn(int x, int y)
-    {
-        return mines.Contains(new Vector2Int(x, y));
-    }
+	public bool HasMineIn(int x, int y) => mines.Contains(new Vector2Int(x, y));
 
-    public int CheckAdjacentMines(int x, int y)
-    {
-        int adjacentMines=0;
-        List<Vector2Int> AdjacentCells = AdjacentOf(x, y);
-        foreach (var cell in AdjacentCells)
-        {
-            if (HasMineIn(cell.x, cell.y))
-            {
-                adjacentMines++;
-            }
-        }
-        return adjacentMines;
-    }
+	public int CheckAdjacentMines(int x, int y) => AdjacentOf(x, y).Count(cell => HasMineIn(cell.x, cell.y));
 
-    public List<Vector2Int> AdjacentOf(int x, int y)
-    {
-        var result = new List<Vector2Int>();
+	public List<Vector2Int> AdjacentOf(int x, int y) {
+		var result = new List<Vector2Int>();
 
-        for (var i = x - 1; i <= x + 1; i++)
-        {
-            for (var j = y - 1; j <= y + 1; j++)
-            {
-                if (i < 0 || j < 0)
-                    continue;
-                if (i == x && j == y)
-                    continue;
+		for (var i = x - 1; i <= x + 1; i++) {
+			for (var j = y - 1; j <= y + 1; j++) {
+				if (i < 0 || j < 0)
+					continue;
+				if (i == x && j == y)
+					continue;
 
-                result.Add(new Vector2Int(i, j));
-            }
-        }
+				result.Add(new Vector2Int(i, j));
+			}
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
