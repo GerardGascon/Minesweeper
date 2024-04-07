3c6bfb18c94b49750c216e6d725008d75649e7f9
ixmas93 <44982516+ixmas93@users.noreply.github.com>
2024-04-07 18:12:03 +0200

    Refactor: Obtain cell value

--- Assets/Runtime/Vista/sladkfjñlasdf.cs
+++ Assets/Runtime/Vista/sladkfjñlasdf.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class sladkfjñlasdf : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject CellPrefab;
+   
+
+    public void UpdateCell(MineSweeper sweeper)
     {
-        
+        string siadjoas = "";
+        for (int i = sweeper.Size.x - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < sweeper.Size.y; j++)
+                siadjoas += ObtainCellValue(sweeper, i, j);
+
+            siadjoas += "\n";
+        }
+
+        //screenText.text = siadjoas;
     }
 
-    // Update is called once per frame
-    void Update()
+    private string ObtainCellValue(MineSweeper sweeper, int i, int j)
     {
-        
+        if (!sweeper.IsRevealed(i, j))
+        {
+            if (sweeper.IsFlagged(i, j))
+                return "A";
+            return "x";
+        }
+
+        return "o";
     }
 }
