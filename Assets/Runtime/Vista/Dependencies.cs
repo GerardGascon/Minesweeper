6bed3f7870d95da82819cd5471759860fec2e4e2
ixmas93 <44982516+ixmas93@users.noreply.github.com>
2024-04-14 11:56:11 +0200

    Test random mines in board

--- Assets/Runtime/Vista/Dependencies.cs
+++ Assets/Runtime/Vista/Dependencies.cs
@@ -6,7 +6,25 @@ public class Dependencies : MonoBehaviour
 {
     private void Start()
     {
-        MineSweeper sweeper = new MineSweeper(new Vector2Int(10, 10), new Vector2Int(0, 0));
+        List<Vector2Int> minas;
+
+        minas = new List<Vector2Int>() 
+        {
+        new Vector2Int(0, 0),
+        new Vector2Int(1, 0),
+        new Vector2Int(0, 2),
+        new Vector2Int(0, 3),
+        new Vector2Int(5, 0),
+        new Vector2Int(2, 4),
+        new Vector2Int(5, 6),
+        new Vector2Int(9, 9),
+        new Vector2Int(2, 6),
+        new Vector2Int(8, 3),
+        new Vector2Int(3, 3),
+        new Vector2Int(3, 5)
+        };
+
+        MineSweeper sweeper = new MineSweeper(new Vector2Int(10, 10), minas);
         Board board = FindObjectOfType<Board>();
         RevealCell revealCell = new RevealCell(board, sweeper);
         board.Setup(sweeper, revealCell);
