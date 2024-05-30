884ebb7b2fe0224174c2dae221b98ec79bccf254
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-05-30 18:32:19 +0200

    test: watered terrain

--- Assets/StardewValley/Domain/Farm.cs
+++ Assets/StardewValley/Domain/Farm.cs
@@ -1,16 +1,17 @@
 
+using System.Collections.Generic;
+using UnityEngine;
+
 public class Farm
 {
-    private bool isWet;
-
+    private List<Vector2Int> wateredCells = new();
 
     public void Water(int x, int y)
     {
-        isWet = true;
+        wateredCells.Add(new Vector2Int(x, y));
     }
 
-    public bool IsWet(int x, int y)
-    {
-        return isWet;
+    public bool IsWet(int x, int y) {
+        return wateredCells.Contains(new Vector2Int(x, y));
     }
 }
