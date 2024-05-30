f4f539ddb6c3e8b2b93e28dbce8e84c7afeaf156
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-05-30 19:04:32 +0200

    feat: basic terrain view

---
+++ Assets/StardewValley/View/Dependencies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace StardewValley.View {
+	public class Dependencies : MonoBehaviour {
+		public Farm farm { private set; get; }
+
+		private void Awake() {
+			farm = new Farm();
+		}
+	}
+}
--- Assets/StardewValley/View/TerrainCell.cs
+++ Assets/StardewValley/View/TerrainCell.cs
@@ -1,17 +1,25 @@
+using System;
+using StardewValley.View;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TerrainCell : MonoBehaviour
-{
+public class TerrainCell : MonoBehaviour {
+    private Farm farm;
+
+    [SerializeField] private Vector2Int cellPosition;
 
     private void Awake()
     {
         GetComponent<Button>().onClick.AddListener(Water);
     }
 
+    private void Start() {
+        farm = FindObjectOfType<Dependencies>().farm;
+    }
+
     private void Water() 
     {
-        
+        farm.Water(cellPosition.x, cellPosition.y);
     }
 
 }
