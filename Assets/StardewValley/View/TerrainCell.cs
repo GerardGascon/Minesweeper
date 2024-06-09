838e96e5b749a315e35528d1aba7ecb48af54f70
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-06-09 12:24:34 +0200

    feat: change color

--- Assets/StardewValley/View/TerrainCell.cs
+++ Assets/StardewValley/View/TerrainCell.cs
@@ -22,6 +22,6 @@ public class TerrainCell : MonoBehaviour {
     }
 
     private void SetWet() {
-        Debug.Log($"Wet {cellPosition} {name}");
+        GetComponent<Image>().color = Color.blue;
     }
 }
