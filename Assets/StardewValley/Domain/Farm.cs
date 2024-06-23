a08965f291f29444541eba0d03cd03a39424e6e2
adrianriverof <adrianriverof@gmail.com>
2024-06-23 12:00:27 +0200

    refactor: use dictionary for check if it's wet

--- Assets/StardewValley/Domain/Farm.cs
+++ Assets/StardewValley/Domain/Farm.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Farm
 {
     
-    private Dictionary<Vector2Int, Cell> djdtfubknfk = new();
+    private Dictionary<Vector2Int, Cell> cells = new();
 
     private List<Vector2Int> wateredCells = new();
     private List<Vector2Int> plantedCells = new();
@@ -15,20 +15,17 @@ public class Farm
     {
         wateredCells.Add(new Vector2Int(x, y));
         
-        djdtfubknfk[new Vector2Int(x, y)] = new Cell(x, y);
-        djdtfubknfk[new Vector2Int(x, y)].isWet = true;
+        cells[new Vector2Int(x, y)] = new Cell(x, y);
+        cells[new Vector2Int(x, y)].isWet = true;
     }
 
     public bool IsWet(int x, int y) {
-        var result = wateredCells.Contains(new Vector2Int(x, y));
-        var sdfs = djdtfubknfk.ContainsKey(new Vector2Int(x,y)) && djdtfubknfk[new Vector2Int(x, y)].isWet;
-        Debug.Assert(result == sdfs);
-        return result;
+        return cells.ContainsKey(new Vector2Int(x,y)) && cells[new Vector2Int(x, y)].isWet;
     }
 
     public void PassDay()
     {
-        foreach (var keyValue in djdtfubknfk.Values)
+        foreach (var keyValue in cells.Values)
         {
             keyValue.isWet = false;
         }
