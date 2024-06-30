64621ababec9f971273bc1f1b03687964770c691
ixmas93 <44982516+ixmas93@users.noreply.github.com>
2024-06-30 12:59:50 +0200

    chore: removed view domain assembly and folder

--- Assets/StardewValley/View/ViewDomain/UpdateCellDomain.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public static class UpdateCellDomain
-{
-    public static void GetCellProperties(bool isWet, bool isPlanted, bool isGrown, int cellStage)
-    {
-        throw new NotImplementedException();
-    }
-}
